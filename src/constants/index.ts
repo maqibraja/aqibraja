b91cc97160f691461fb12b6da20ae4086acb07e0
Aqib Raja <maqibraja@hotmail.com>
2024-06-22 15:03:11 -0400

    kermet

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -80,10 +80,10 @@ const experiences: TExperience[] = [
     iconBg: "#383E56",
     date: "Jan 2022 - Jan 2023",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "The Stack Application is ",
+      "the best ux",
+      "responsice design in code reviews",
+      "Participating in code reviews",
     ],
   },
   {
