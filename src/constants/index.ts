2ca227401c87c0b38031bc07d25d002e72ce1c78
Aqib Raja <maqibraja@hotmail.com>
2024-06-24 10:43:46 -0400

    kevin push

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -82,7 +82,7 @@ const experiences: TExperience[] = [
     points: [
       "The Stack Application is ",
       "the best ux",
-      "responsice design in code reviews",
+      "responsice design in our code reviews",
       "Participating in code reviews",
     ],
   },
