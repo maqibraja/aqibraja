edb8c30d387a3bc6981b7d3210003dfe145353c7
Aqib Raja <maqibraja@hotmail.com>
2024-07-12 08:52:50 -0400

    push

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -82,7 +82,7 @@ const experiences: TExperience[] = [
     points: [
       "The Stack Application is ",
       "the best ux",
-      "responsice design infsfdf sky ourr when auroa code reveigfdgfdews",
+      "responsice design infsfdf fsdfsdsky ourr when auroa code reveigfdgfdews",
       "Participating in code reviews of analog of data of  using github",
     ],
   },
